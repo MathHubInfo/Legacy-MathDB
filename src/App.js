8872a2f757ba55548a5e2bc236393752f302e16d
Katja Berčič <katja.bercic@gmail.com>
2018-12-18 13:05:50 +0100

    Populate the react-table with some data

--- src/App.js
+++ src/App.js
@@ -1,28 +1,57 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import ReactTable from 'react-table';
 import './App.css';
+import db from './cmo.json';
+
+const getAuthors = (collectionId) => {
+    return db.collection_author.filter((ca) => { 
+        return ca.c_id === collectionId;
+    }).map((ca) => {
+        return db.author.filter((a) => { return a.id === ca.a_id; })[0]
+    })
+}
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+    
+    render() {
+      
+        console.log(db);
+
+        const columns = [{
+            Header: 'Name',
+            accessor: 'name',
+            Cell: props => <CollectionName text={props.value.text} url={props.value.url} />
+        }, {
+            Header: '# of objects',
+            accessor: 'size'
+        }, {
+            Header: 'Author(s)',
+            accessor: 'authors',
+            Cell: props => <Authors data={props.value} />
+        }]
+
+        const data = db.collection.map((c) => {
+            return {
+                name: {text: c.name, url: c.url},
+                authors: getAuthors(c.id),
+                size: c.size
+            }
+        })
+
+        return (
+            <div className="App">
+                <ReactTable data={data} columns={columns} />
+            </div>
+        );
+    }
+}
+
+function CollectionName(props) {
+    return <a href={props.url}>{props.text}</a>;
+}
+
+function Authors(props) {
+   return <span>a</span> 
 }
 
 export default App;
